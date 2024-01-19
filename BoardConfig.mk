BlissRoms-Devices/android_device_asus_obiwan

obiwan: Adjust and Reserve partition size for vanilla variant

BoardConfig.mk
@@ -170,10 +170,13 @@ BOARD_DTBOIMG_PARTITION_SIZE := 25165824
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 104857600
 
-ifneq ($(filter vanilla,$(BLISS_BUILD_VARIANT)),)
-BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1629145600
-BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 429145600
-BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 429145600
+ifneq ($(BLISS_BUILD_VARIANT),gapps)
+BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE ?= 1957691392
+BOARD_PRODUCTIMAGE_EXTFS_INODE_COUNT ?= -1
+BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE ?= 94371840
+BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT ?= -1
+BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE ?= 94371840
+BOARD_SYSTEM_EXTIMAGE_EXTFS_INODE_COUNT ?= -1
 endif
 
 BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4

ApolloOS-Devices/device_asus_obiwan

Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

BoardConfig.mk
@@ -170,10 +170,13 @@ BOARD_DTBOIMG_PARTITION_SIZE := 25165824
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 104857600
 
-ifneq ($(filter vanilla,$(BLISS_BUILD_VARIANT)),)
-BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1629145600
-BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 429145600
-BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 429145600
+ifneq ($(APOLLO_BUILD_VARIANT),gapps)
+BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE ?= 1957691392
+BOARD_PRODUCTIMAGE_EXTFS_INODE_COUNT ?= -1
+BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE ?= 94371840
+BOARD_SYSTEMIMAGE_EXTFS_INODE_COUNT ?= -1
+BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE ?= 94371840
+BOARD_SYSTEM_EXTIMAGE_EXTFS_INODE_COUNT ?= -1
 endif
 
 BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4